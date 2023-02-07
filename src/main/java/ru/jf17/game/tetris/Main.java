42713981405cce1f0db9fca26727ee6ffd34683d
alex <javafirst17@gmail.com>
2023-02-07 21:44:07 +0300

    Create Game logic

---
+++ src/main/java/ru/jf17/game/tetris/Main.java
@@ -0,0 +1,43 @@
+package ru.jf17.game.tetris;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+    private JLabel statusbar;
+
+    public Main() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        statusbar = new JLabel(" 0");
+        add(statusbar, BorderLayout.SOUTH);
+
+        var board = new Board(this);
+        add(board);
+        board.start();
+
+        setTitle("Tetris");
+        setSize(400, 800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    JLabel getStatusBar() {
+
+        return statusbar;
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            var game = new Main();
+            game.setVisible(true);
+        });
+    }
+}
